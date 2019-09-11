06ae2107fc8460b9a20296f70b50739ca0b8238d
Wallison Rocha <wallisonrocha2008@gmail.com>
2019-09-10 23:28:48 -0300

    Cadastrar e mostrar produto individualmente

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,26 @@
-class Product < ApplicationController 
+class ProductsController < ApplicationController 
 
     def index
     
     end
 
     def show
-    
+        @product = Product.find(params[:id])
     end
 
     def new
-    
+        @product = Product.new
     end
 
     def create
-    
+        @product = Product.new(product_params)
+        @product.old_price = @product.price
+        if @product.save
+            flash[:success] = "Produto cadastrado com sucesso!"
+            redirect_to @product
+        else
+            render 'new'
+        end
     end
 
     def edit
